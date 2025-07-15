593c36db2a2e276dba0b16c24b837b995166404c
lupingguo <hold7habits@gmail.com>
2025-07-15 18:39:49 +0800

    --feat: update data

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/sapaude/go_sapaude_backend_admin
 go 1.24.1
 
 require (
-	github.com/golang-jwt/jwt/v5 v5.2.2
+	github.com/golang-jwt/jwt/v5 v5.2.3
 	github.com/google/wire v0.6.0
 	github.com/labstack/echo/v4 v4.13.4
 	github.com/sapaude/go-shims/shim v0.0.0-20250711130206-cfba225a8dca
 	github.com/sapaude/go-shims/x/log v0.0.0-20250711130206-cfba225a8dca
 	github.com/sirupsen/logrus v1.9.3
-	golang.org/x/crypto v0.38.0
+	golang.org/x/crypto v0.40.0
 	gopkg.in/yaml.v3 v3.0.1
 	gorm.io/driver/mysql v1.6.0
 	gorm.io/gorm v1.30.0
@@ -17,7 +17,7 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
@@ -27,13 +27,9 @@ require (
 	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/net v0.40.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.25.0 // indirect
-	golang.org/x/time v0.11.0 // indirect
+	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
+	golang.org/x/time v0.12.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
-
-replace github.com/sapaude/go-shims/x/log => /private/data/projects/github.com/sapadue/go-shims/x/log
-
-replace github.com/sapaude/go-shims/shim => /private/data/projects/github.com/sapadue/go-shims/shim
--- main.go
+++ main.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-	"context"
+    "context"
 
-	"github.com/sapaude/go-shims/x/log"
-	"github.com/sapaude/go_sapaude_backend_admin/conf"
-	"github.com/sapaude/go_sapaude_backend_admin/routes"
-	"github.com/sirupsen/logrus"
+    "github.com/sapaude/go-shims/x/log"
+    "github.com/sapaude/go_sapaude_backend_admin/conf"
+    "github.com/sapaude/go_sapaude_backend_admin/routes"
+    "github.com/sirupsen/logrus"
 )
 
 func main() {
-	logrus.SetFormatter(&logrus.JSONFormatter{
-		TimestampFormat:   `2006-01-02 15:04:05.000`,
-		DisableTimestamp:  false,
-		DisableHTMLEscape: false,
-		DataKey:           "",
-		FieldMap:          nil,
-		CallerPrettyfier:  nil,
-		PrettyPrint:       false,
-	})
-	background := context.Background()
-	ctx := context.WithValue(background, "key1", "val1")
-	ctx = context.WithValue(ctx, "uin", 100)
-	srvLog := logrus.WithContext(ctx).WithFields(logrus.Fields{
-		"modName": "fieldMod",
-		"key1":    ctx.Value("key1"),
-		"uin":     ctx.Value("uin"),
-	})
-	log.SetDefaultLogger(srvLog.Logger)
-	if err := conf.LoadConfig("conf/app.yml", "conf/route.yml"); err != nil {
-		logrus.Fatalf("config load error: %v", err)
-	}
-	srvLog.Infof("info what ")
+    logrus.SetFormatter(&logrus.JSONFormatter{
+        TimestampFormat:   `2006-01-02 15:04:05.000`,
+        DisableTimestamp:  false,
+        DisableHTMLEscape: false,
+        DataKey:           "",
+        FieldMap:          nil,
+        CallerPrettyfier:  nil,
+        PrettyPrint:       false,
+    })
+    background := context.Background()
+    ctx := context.WithValue(background, "key1", "val1")
+    ctx = context.WithValue(ctx, "uin", 100)
+    srvLog := logrus.WithContext(ctx).WithFields(logrus.Fields{
+        "modName": "fieldMod",
+        "key1":    ctx.Value("key1"),
+        "uin":     ctx.Value("uin"),
+    })
+    // log.SetDefaultLogger(srvLog.Logger)
+    if err := conf.LoadConfig("conf/app.yml", "conf/route.yml"); err != nil {
+        logrus.Fatalf("config load error: %v", err)
+    }
+    srvLog.Infof("info what ")
 
-	// 创建Echo服务
-	echoServer := routes.NewEchoServer(InitBackendAdminImpl())
+    // 创建Echo服务
+    echoServer := routes.NewEchoServer(InitBackendAdminImpl())
 
-	// 启动服务
-	addr := conf.AppConfig.Server.Address
-	log.Infof("Starting server at %s", addr)
-	if err := echoServer.Start(addr); err != nil {
-		log.Fatalf("server error: %v", err)
-	}
+    // 启动服务
+    addr := conf.AppConfig.Server.Address
+    log.Infof("Starting server at %s", addr)
+    if err := echoServer.Start(addr); err != nil {
+        log.Fatalf("server error: %v", err)
+    }
 }
